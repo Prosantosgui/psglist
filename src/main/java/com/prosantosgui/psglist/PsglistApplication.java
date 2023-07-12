c3a95de779e22d8826cf30c35425020025f9099a
Guilherme dos Santos <prfsantosgui546@gmail.com>
2023-07-12 16:33:48 -0300

    Projeto iniciado

---
+++ src/main/java/com/prosantosgui/psglist/PsglistApplication.java
@@ -0,0 +1,13 @@
+package com.prosantosgui.psglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsglistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prosantosgui/psglist/PsglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosantosgui.psglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
